99d0b5212f62e5fdb35b76adea21a554227e1d96
hovnySiLenCe <hovny@qq.com>
2025-08-14 17:58:32 +0800

    addQtThread

--- Cube_Explorer_SPFA_with_Qt/Algorithm/MultiSolver.h
+++ Cube_Explorer_SPFA_with_Qt/Algorithm/MultiSolver.h
@@ -1,28 +1,29 @@
 #pragma once
-#include <thread>
-#include <mutex>
-#include <vector>
 #include <QDebug>
-#include "threadSafeKociemba.h"
-#include "CubePoseTransformer.h"
-#include "MultiSolver.h"
-#include "CubeExplorerSPFA.h"
+#include <QObject>
+#include <QEventLoop>
+#include <QTimer>
+#include "MyThread.h"
 
-class MultiSolver {
+#define	MAX_THREAD_NUM 2
+
+class MultiSolver : public QObject {
+	Q_OBJECT
+
 private:
-	threadSafeKociemba* kociembaSolver[24];
-	CubeExplorerSPFA* ansCubeExplorerSPFA;
-	CubeExplorerSPFA* cubeExplorerSPFA[24];
-	CubePoseTransformer* cubePoseTransformer[24];
-	string cubeStatus;
-	int ansTime;
-	void GetSinglePosePath(int poseId);
-	
+	MyThread* m_thread[24];
+	int ansTime, ansId;
+	int finishedCnt;
 
 public:
+	MultiSolver();
 	CubeExplorerSPFA* GetMultiThreadPath(const string& str, int threshold=800, int timeoutTime=200);
-	void InitSolver();
-	//ThreadSolver() {
-	//	InitSolver();
-	//}
+	~MultiSolver();
+
+public slots:
+	void slot_threadFinished(int id, int costTime);
+
+signals:
+	void allThreadsFinished();
+
 };
